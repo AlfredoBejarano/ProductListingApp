b51451caf0ef9df1e32b4b78794c66fd1fbac0c9
Alfredo Bejarano <alfredocbejarano@gmail.com>
2018-10-12 21:03:38 -0500

    Create AppDatabase.kt
    Class that defines the app local database structure using Room

---
+++ app/src/main/java/me/alfredobejarano/productlisting/data/AppDatabase.kt
@@ -0,0 +1,43 @@
+package me.alfredobejarano.productlisting.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import me.alfredobejarano.productlisting.BuildConfig
+
+/**
+ *
+ * Class defining all the accessors for the app database entities.
+ *
+ * @author Alfredo Bejarano
+ * @since 12/10/2018 - 08:53 PM
+ * @version 1.0
+ **/
+@Database(entities = [Session::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    companion object {
+        // Used for singleton initialization
+        @Volatile
+        private var instance: AppDatabase? = null
+
+        /**
+         * Retrieves the instance of the app database.
+         * @param ctx The context requesting the app database.
+         */
+        fun getInstance(ctx: Context) = instance ?: synchronized(this) {
+            instance ?: buildDatabase(ctx).also { instance = it }
+        }
+
+        /**
+         * Builds an instance of the app database using Room.
+         * @param ctx The context building the database instance.
+         */
+        private fun buildDatabase(ctx: Context) =
+            Room.databaseBuilder(
+                ctx,
+                AppDatabase::class.java,
+                "{${BuildConfig.APPLICATION_ID}-db}"
+            ).fallbackToDestructiveMigration().build()
+    }
+}
