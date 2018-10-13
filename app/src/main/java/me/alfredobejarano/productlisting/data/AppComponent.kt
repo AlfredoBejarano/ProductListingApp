8dbf0130fae348a1c3ae8a78051d9f28c9c5d507
Alfredo Bejarano <alfredocbejarano@gmail.com>
2018-10-12 21:59:36 -0500

    Create AppComponent.kt
    Dagger component that builds the dependency injection graph

---
+++ app/src/main/java/me/alfredobejarano/productlisting/data/AppComponent.kt
@@ -0,0 +1,31 @@
+package me.alfredobejarano.productlisting.data
+
+import dagger.Component
+import javax.inject.Singleton
+
+/**
+ *
+ * Dagger component that defines the app injection methods.
+ *
+ * @author Alfredo Bejarano
+ * @since 12/10/2018 - 09:27 PM
+ * @version 1.0
+ **/
+@Singleton
+@Component(modules = [WebserviceModule::class, LoginRepositoryModule::class])
+interface AppComponent {
+    /**
+     * Provides injection for a [Webservice] implementation.
+     */
+    fun provideWebservice(): Webservice
+
+    /**
+     * Provides injection for [SessionDao] class.
+     */
+    fun provideSessionDao(): SessionDao
+
+    /**
+     * Provides injection for the [LoginRepository] class.
+     */
+    fun provideLoginRepository(): LoginRepository
+}
