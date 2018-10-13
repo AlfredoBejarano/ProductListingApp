9671e749e49bb555425672d1d1d43f3c93fa7d43
Alfredo Bejarano <alfredocbejarano@gmail.com>
2018-10-12 20:04:01 -0500

    Create Session.kt
    Entity class for storing the user session locally

---
+++ app/src/main/java/me/alfredobejarano/productlisting/data/Session.kt
@@ -0,0 +1,22 @@
+package me.alfredobejarano.productlisting.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ *
+ * Entity class that defines the necessary values
+ * for a user session as the expiration date and
+ * access token.
+ *
+ * @author Alfredo Bejarano
+ * @since 12/10/2018 - 07:59 PM
+ * @version 1.0
+ **/
+@Entity(tableName = "sessions_table")
+data class Session(
+    @PrimaryKey(autoGenerate = false)
+    val id: Int = 1,
+    var token: String,
+    var expirationTime: Long
+)
