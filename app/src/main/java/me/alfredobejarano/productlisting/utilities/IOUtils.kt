9430177921edc976a8d9df8b550e3cc30b3306d3
Alfredo Bejarano <alfredocbejarano@gmail.com>
2018-10-13 00:57:04 -0500

    Create IOUtils.kt
    Utils class that provides functions for running code in a worker thread

---
+++ app/src/main/java/me/alfredobejarano/productlisting/utilities/IOUtils.kt
@@ -0,0 +1,24 @@
+package me.alfredobejarano.productlisting.utilities
+
+import androidx.lifecycle.ViewModel
+import java.util.concurrent.Executors
+
+/**
+ *
+ * Utils class that provides functions to handle
+ * work on different threads easily
+ *
+ * @author Alfredo Bejarano
+ * @since 13/10/2018 - 12:44 AM
+ * @version 1.0
+ **/
+/**
+ * Single threaded executor that allows job being done in a worker thread.
+ */
+private val IO_EXECUTOR = Executors.newSingleThreadExecutor()
+
+/**
+ * Extension function for the [ViewModel] class that allows
+ * executing a function in a worker thread.
+ */
+fun ViewModel.runOnIOThread(f: () -> Unit) = IO_EXECUTOR.execute(f)
