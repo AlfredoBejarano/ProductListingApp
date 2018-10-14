eda7a4cc70f967ebc1557498141aefb0946b3624
Alfredo Bejarano <alfredocbejarano@gmail.com>
2018-10-14 01:55:23 -0500

    Update SessionRepository.kt
    Add function for retrieving a User profile from the server using an access token

--- app/src/main/java/me/alfredobejarano/productlisting/data/SessionRepository.kt
+++ app/src/main/java/me/alfredobejarano/productlisting/data/SessionRepository.kt
@@ -1,5 +1,11 @@
 package me.alfredobejarano.productlisting.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.HttpException
+import retrofit2.Response
 import javax.inject.Inject
 
 /**
@@ -11,12 +17,54 @@ import javax.inject.Inject
  * @since 12/10/2018 - 10:02 PM
  * @version 1.0
  **/
-class SessionRepository @Inject constructor(private val dao: SessionDao) {
+class SessionRepository @Inject constructor(
+    private val dao: SessionDao,
+    private val webservice: Webservice
+) {
+    /**
+     * Reference to the token performing the profile fetching.
+     */
+    private var token: String = ""
+
+    /**
+     * Callback object that fetches a user profile from the cloud.
+     */
+    private val profileCallback = object : Callback<Wrapper<User>> {
+        /**
+         * Reports that fetching a profile was not possible.
+         */
+        override fun onFailure(call: Call<Wrapper<User>>, t: Throwable) = sessionFetched.postValue(false)
+
+        /**
+         * Stores the retrieved user profile as a session in the local storage databse.
+         */
+        override fun onResponse(c: Call<Wrapper<User>>, r: Response<Wrapper<User>>) = r.body()?.data?.let {
+            dao.persistSession(
+                Session(it.id, "${it.name} ${it.surName}", token, it.email, it.image)
+            )
+            sessionFetched.postValue(true)
+        } ?: run {
+            onFailure(c, HttpException(r))
+        }
+    }
+    /**
+     * [MutableLiveData] object that provides observation to the UI controllers
+     * about the status of fetching and storing a user session in the device.
+     */
+    var sessionFetched: MutableLiveData<Boolean> = MutableLiveData()
+    /**
+     * [LiveData] object that provides observation to the UI controllers
+     * about the stored session in the device local database.
+     */
+    var currentSession: LiveData<List<Session>> = MutableLiveData()
+
     /**
      * Fetches the current session from the database.
-     * @return [androidx.lifecycle.LiveData] object containing the [Session] object.
+     * @return [LiveData] object containing the [Session] object within a [List].
      */
-    fun retrieveSession() = dao.getCurrentSession()
+    fun retrieveSession() {
+        currentSession = dao.getCurrentSession()
+    }
 
     /**
      * Deletes the current session from the database.
@@ -25,7 +73,17 @@ class SessionRepository @Inject constructor(private val dao: SessionDao) {
 
     /**
      * Stores a session from a response.
-     * @param response The valid response object from the server.
+     * @param session The session to be persisted.
      */
-    fun persistSession(session: Session) = dao.persistSession(session)
+    private fun persistSession(session: Session) = dao.persistSession(session)
+
+    /**
+     * Uses an [access token][String] to retrieve the details of a User.
+     * @param accessToken The token for the user, retrieved via login.
+     * @see LoginRepository
+     */
+    fun fetchUserFromToken(accessToken: String) {
+        token = accessToken
+        webservice.fetchUserProfile(accessToken).enqueue(profileCallback)
+    }
 }
