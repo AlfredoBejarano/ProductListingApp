af297174b6f939b946f498e9a0c6592e49804fb9
Alfredo Bejarano <alfredocbejarano@gmail.com>
2018-10-14 01:53:38 -0500

    Update Webservice.kt
    Add API definitions for retrieving a list of post objects and fetching the user profile using an access token

--- app/src/main/java/me/alfredobejarano/productlisting/data/Webservice.kt
+++ app/src/main/java/me/alfredobejarano/productlisting/data/Webservice.kt
@@ -4,6 +4,7 @@ import me.alfredobejarano.productlisting.BuildConfig
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Headers
 
 /**
@@ -22,4 +23,19 @@ interface Webservice {
     @GET("login")
     @Headers(BuildConfig.HeaderLang, BuildConfig.HeaderAppVersion)
     fun performLoginRequest(@Body request: LoginRequest): Call<Wrapper<Login>>
+
+    /**
+     * Function that defines how to retrieve a user profile using
+     * an access token.
+     */
+    @GET("user/profile")
+    @Headers(BuildConfig.HeaderLang, BuildConfig.HeaderAppVersion)
+    fun fetchUserProfile(@Header("Authorization") accessToken: String): Call<Wrapper<User>>
+
+    /**
+     * Fetches a list of posts from the cloud.
+     */
+    @GET("post/all")
+    @Headers(BuildConfig.HeaderLang, BuildConfig.HeaderAppVersion)
+    fun fetchPosts(): Call<Wrapper<PostsList>>
 }
