ee65056c664049e929fdbbf9c8a98537e7ac698a
Alfredo Bejarano <alfredocbejarano@gmail.com>
2018-10-14 01:21:22 -0500

    Create PostsList.kt
    Model class that represents the results from a server query requesting a data set of posts

---
+++ app/src/main/java/me/alfredobejarano/productlisting/data/PostsList.kt
@@ -0,0 +1,54 @@
+package me.alfredobejarano.productlisting.data
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+/**
+ *
+ * Data class that serves as the model for the
+ * list of post functionality, this class defines
+ * the JSON structure of a request for getting all
+ * the posts from the backend.
+ *
+ * @author Alfredo Bejarano
+ * @since 14/10/2018 - 01:11 AM
+ * @version 1.0
+ **/
+class PostsList(
+    @Expose
+    @SerializedName("to")
+    val to: Int,
+    @Expose
+    @SerializedName("from")
+    val from: Int,
+    @Expose
+    @SerializedName("total")
+    val total: Int,
+    @Expose
+    @SerializedName("per_page")
+    val perPage: Int,
+    @Expose
+    @SerializedName("path")
+    val path: String,
+    @Expose
+    @SerializedName("current_page")
+    val currentPage: Int,
+    @Expose
+    @SerializedName("last_page")
+    val lastPage: String,
+    @Expose
+    @SerializedName("last_page_url")
+    val lastPageURL: String,
+    @Expose
+    @SerializedName("next_page_url")
+    val nextPageURL: String,
+    @Expose
+    @SerializedName("list_post")
+    val results: List<Post>,
+    @Expose
+    @SerializedName("ﬁrst_page_url")
+    val firstPageURL: String,
+    @Expose
+    @SerializedName("prev_page_url")
+    val previousPageURL: String
+)
